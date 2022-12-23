63efa332716dd490284be52731167c73b64eba72
Meagan St Clair <stclair.meagan@gmail.com>
2022-12-22 20:59:49 -0600

    adding files

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,36 @@
+// import { Icon, IconButton, Typography } from '@material-ui/core'
+import React from 'react'
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
+
+//Allows us to use the styles created in Styles.js after calling below (line 12)
+import useStyles from './styles'; // hook
+
+// Layout of each product (product card)
+// since product is a subcomponent of Products.jsx, we can call product from there (passes on props)
+const Product = ({ product }) => {
+    const classes = useStyles(); // complete hook 
+  return (
+    <Card className={classes.root}>
+        <CardMedia className={classes.media} image={product.image} title={product.name}/>
+        <CardContent>
+            <div className={classes.cardContent}>
+                <Typography variant="h5" gutterBottom>
+                    {product.name}
+                </Typography>
+                <Typography variant="h5">
+                    {product.price}
+                </Typography>
+            </div>
+            <Typography variant="body2" color="textSecondary">{product.description}</Typography>
+        </CardContent>
+        <CardActions disableSpacing className={classes.CardActions}>
+            <IconButton aria-label="Add to Cart">
+                <AddShoppingCart />
+            </IconButton>
+        </CardActions>
+    </Card>
+  )
+}
+
+export default Product
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+
+import Product from './Product/Product';
+import useStyles from './styles';
+
+// products array containing list of diff products (name & description)
+const products = [
+    { id: 1, name: 'Shoes', description: 'Running Shoes', price: '$5', image: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/49e5514bb83e4c5d8bf5ad9100ef2815_9366/Ultraboost_22_Running_Shoes_White_GX5459_01_standard.jpg' },
+    { id: 2, name: 'MacBook', description: 'Apple MacBook', price: '$10', image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/macbook-air-space-gray-select-201810?wid=1200&hei=630&fmt=jpeg&qlt=95&.v=1664472289661' },
+];
+
+// Create Products component
+const Products = () => {
+    const classes = useStyles();
+
+    return (
+        <main className={classes.content}>
+            <div className={classes.toolbar}/>
+            <Grid container justify="center" spacing={4}>
+                {products.map((product) => (     // get each product & then return something specific
+                    // item grid with key (to loop we need id), gird contains Product component
+                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>   
+                        <Product product={product}/>
+                    </Grid>  
+                    // pass product via product prop which we will loop over and send each one by one & give product.jsx access to id, name, desc, etc
+                ))}
+            </Grid>
+        </main>
+    )
+}
+
+export default Products;
